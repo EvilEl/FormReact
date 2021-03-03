1a40cc04451e0b63ac56443a325d4f15326d6a3a
Mikhail <enc0ru@rambler.ru>
2021-03-03 21:14:25 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/app";
+import App from "./components/app/index";
 
 import { HashRouter as Router } from "react-router-dom";
 
